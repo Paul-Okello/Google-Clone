2f8bd2cf84827b3259cedc74ccb1fa2380009fab
Paul Okello <paulokello273@gmail.com>
2021-04-17 15:36:11 +0300

    Avatar component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
-function Avatar({ url }) {
+function Avatar({ url, className }) {
   return (
     <img
       loading="lazy"
       src={url}
       alt="profile picture"
-      className=" rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110"
+      className={` rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
     />
   );
 }
